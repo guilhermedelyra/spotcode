85e5895a411a3680e83f11e61b37af999083c290
Guilherme de Lyra <guilyra12@gmail.com>
2018-12-11 13:13:35 +0000

    Aula 2 - Dashboard

--- spotcode/config/routes.rb
+++ spotcode/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+  	root to: "dashboard#index", as: :authenticated_root
+  end
+
+  unauthenticated :user do
+  	root to: "home#index"
+  end
 end
